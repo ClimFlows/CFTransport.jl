a169b7bf68e79ade807507ddf171cd07e6fd0067
Thomas Dubos <thomas.dubos@lmd.ipsl.fr>
2024-06-03 11:13:36 +0200

    Stencils

--- src/CFTransport.jl
+++ src/CFTransport.jl
@@ -1,5 +1,59 @@
 module CFTransport
 
-# Write your package code here.
+using ManagedLoops: @loops, @vec
 
+export GodunovScheme, VanLeerScheme
+export concentrations!, slopes!, fluxes!, FV_update!
+
+macro fast(code)
+    debug = haskey(ENV, "GF_DEBUG") && (ENV["GF_DEBUG"]!="")
+    return debug ? esc(code) : esc(quote @inbounds $code end)
 end
+
+include("julia/stencil.jl")
+
+"""
+    abstract type OneDimOp{dim,rank} end
+
+One-dimensional operator acting on dimension `dim` of arrays of rank `rank`.
+"""
+abstract type OneDimOp{Dim,Rank} end
+
+# excludes from range `1:N` `a` items at the start, and `b` items at the end,
+struct Crop{a,b} end
+crop(a,b)=Crop{a,b}()
+(::Crop{a,b})(N::Int) where {a,b} = (1+a):(N-b)
+
+# enforces restriction `r` on dimension `k` :
+# replaces dimension `dim_k` by the result of r(size(dim_k)),
+# e.g. 1:N => 2:N-1
+@inline restrict(::OneDimOp{1,1}, r::Fun, (dim,) )      where Fun =  r(length(dim))
+@inline restrict(::OneDimOp{1,2}, r::Fun, (dim1,dim2) ) where Fun = (r(length(dim1)), dim2)
+@inline restrict(::OneDimOp{2,2}, r::Fun, (dim1,dim2) ) where Fun = (dim1, r(length(dim2)))
+
+# applies function `step` related to operator `op` to `arrays` on `backend`,
+# after restricting dimension `dim` to only(size(dim))
+@inline function invoke(step::Fun, op::OneDimOp{dim,2}, backend, ranges, only::Only, arrays) where {dim,Fun,Only}
+    step = expand_stencil{dim, 2}(step)
+    ranges = restrict(op, only, ranges)
+    inkoke_step(backend, ranges, step, op, arrays)
+end
+
+@loops function invoke_step(_, ranges, step, op, arrays)
+    let (ri,rj) = ranges
+        for j in rj
+            @vec for i in ri
+                @inline step((i,j), op, arrays)
+            end
+        end
+    end
+end
+
+@inline half(x) = @fastmath x/2
+
+#include("julia/finite_volume.jl")
+#include("julia/godunov.jl")
+#include("julia/vanleer.jl")
+#include("julia/limiters.jl")
+
+end # module
---
+++ src/julia/stencil.jl
@@ -0,0 +1,111 @@
+"""
+A one-dimensional stencil is a function/closure/callable of the form
+
+    function stencil(i, params, arrays)
+        a, b, ... = arrays
+        a[i] = expression(params, b[i], b[i+1], b[i-1], ...)
+    end
+
+A two-dimensional stencil is a function/closure/callable of the form
+
+    function stencil((i,j), params, arrays)
+        a, b, ... = arrays
+        a[i,j] = expression(params, b[i,j], b[i+1,j], b[i,j+1], ...)
+    end
+
+A stencil acts at a single index. For performance, stencils should be @inline and use @inbounds.
+"""
+abstract type Stencil <: Function end
+
+"""
+    stencil = expand_stencil(dim, rank, stencil1)
+
+Returns a `stencil` of rank `rank` that applies the one-dimensional
+stencil `stencil1` to dimension `dim`.
+
+    stencil = apply_stencil(dims, rank, stencilN)
+
+Returns a `stencil` of rank `rank` that applies the `N`-dimensional
+stencil `stencilN` to the `N`-tuple of dimensions `dims`.
+
+`stencil` can be passed to `forall` :
+
+    forall(stencil, backend, ranges, arrays)
+
+where `arrays` is the tuple of arrays on which the stencil acts
+and `ranges` is more or less `axes(a)` with `a` one of the arrays,
+except that the range over the stencil index shoud be reduced
+according to the stencil width to avoid out-of-bounds accesses.
+
+See also [`Stencil`](@ref)
+"""
+struct expand_stencil{Index, Indices, Fun} <: Function
+    call::Fun
+    @inline expand_stencil{index,rank}(call::Fun) where {index, rank, Fun<:Function} = new{index, rank, Fun}(call)
+end
+
+@inline function (stencil::expand_stencil)(i, arg, arrays)
+    i, arrays = @inline slices(stencil, i, arrays)
+    @inline stencil.call(i, arg, arrays)
+end
+
+const xps=expand_stencil
+
+slices(::xps{1,1}, i, arrays)       = i, arrays
+
+slices(::xps{1,2}, (i,j), arrays)   = i, map( x->(@inbounds @view x[:,j]),   arrays)
+slices(::xps{2,2}, (i,j), arrays)   = j, map( x->(@inbounds @view x[i,:]),   arrays)
+
+slices(::xps{1,3}, (i,j,k), arrays) = i, map( x->(@inbounds @view x[:,j,k]), arrays)
+slices(::xps{2,3}, (i,j,k), arrays) = j, map( x->(@inbounds @view x[i,:,k]), arrays)
+slices(::xps{3,3}, (i,j,k), arrays) = k, map( x->(@inbounds @view x[i,j,:]), arrays)
+
+struct ApplyStencil{Index, Indices, S} <: Function
+    stencil::S
+end
+
+"""
+    kernel = apply_stencil(dim, rank, stencil)
+
+Returns a `kernel` that applies a one-dimensional stencil
+to dimension `dim` of input/output arrays of rank `rank`
+
+    kernel = apply_stencil(dims, rank, stencil)
+
+Returns a `kernel` that applies a multi-dimensional stencil
+to dimensions 'dims' of input/output arrays of rank `rank`.
+
+`kernel` can be called directly or offloaded to a backend :
+
+    kernel(ranges, arrays)
+    (kernel=>backend)(ranges, arrays)
+
+where `ranges` is more or less `axes(a)` with `a` one of the arrays,
+except that the range over the stencil index shoud be reduced
+according to the stencil width to avoid out-of-bounds accesses.
+
+See also [`Stencil`](@ref)
+"""
+apply_stencil(index, indices, stencil::S) where S = ApplyStencil{index, indices, S}(stencil)
+
+# Apply 1D stencil to first dimension of 2-dimensional arrays
+@inline function (kernel::ApplyStencil{1, 2})((irange, jrange), arrays)
+    for j in jrange
+        @simd for i in irange
+            slices = tuple(((@inbounds @view x[:,j]) for x in arrays)...)
+            kernel.stencil(i, slices)
+        end
+    end
+    nothing
+end
+
+# Apply 1D stencil to second dimension of 2-dimensional arrays
+@inline function (kernel::ApplyStencil{2, 2})((irange, jrange), arrays)
+    for j in jrange
+        @simd for i in irange
+            slices = tuple(((@inbounds @view x[i,:]) for x in arrays)...)
+            kernel.stencil(j, slices)
+        end
+    end
+    nothing
+end
